f9ad279dd7e1278ba29529dab61b49fc02714965
David Huang <dhuang5252@gmail.com>
2021-04-26 20:11:04 -0500

    remove content scripts

--- src/scripts/youtubeHome.js
+++ src/scripts/youtubeHome.js
@@ -1,13 +1,24 @@
 import { getAllVideosOnHomePage, getVideoTitle, getVideoChannel } from './utils/youtube'
 
-const ytdRichItemRendererList = getAllVideosOnHomePage()
-for(let i = 0; i < ytdRichItemRendererList.length; i++) {
-  const ytdRichItemRenderer = ytdRichItemRendererList[i]
-  const videoMetaData = ytdRichItemRenderer.querySelector('#content ytd-rich-grid-media #dismissible #details #meta')
-  if (!videoMetaData) {
-    continue
+getAllVideoMetaData = () => {
+  const videoMetaDataList = []
+  const ytdRichItemRendererList = getAllVideosOnHomePage()
+  for(let i = 0; i < ytdRichItemRendererList.length; i++) {
+    const ytdRichItemRenderer = ytdRichItemRendererList[i]
+    const videoMetaData = ytdRichItemRenderer.querySelector('#content ytd-rich-grid-media #dismissible #details #meta')
+    if (!videoMetaData) {
+      continue
+    }
+    const videoChannel = getVideoChannel(videoMetaData)
+    const videoTitle = getVideoTitle(videoMetaData)
+    console.log(videoChannel, videoTitle)
+    videoMetaDataList.push({
+      videoChannel,
+      videoTitle
+    })
   }
-  const videoChannel = getVideoChannel(videoMetaData)
-  const videoTitle = getVideoTitle(videoMetaData)
-  console.log(videoChannel, videoTitle)
+}
+
+ handleBlur = (hideListManager) => {
+  videoMetaDataList = getAllVideoMetaData()
 }
