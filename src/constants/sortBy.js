db5914c18817bc04d1f452e66de016cfd3efc629
David Huang <dhuang5252@gmail.com>
2021-09-12 13:07:41 -0500

    remove unused constants

--- src/constants/sortBy.js
+++ src/constants/sortBy.js
@@ -1,29 +1,5 @@
-// TODO: rename file
-
 export const sortBy = {
   OLD: 'Old',
   NEW: 'New',
   ABC: 'Abc',
-}
-
-export const youTubePage = {
-  HOME: 'home',
-  WATCH: 'watch',
-  CHANNEL: 'channel',
-}
-
-export const contentRootQuerySelector = {
-  home: '#contents',
-  watch: 'ytd-watch-flexy #columns #primary #related #contents',
-}
-
-export const videoTagName = {
-  home: 'ytd-rich-item-renderer',
-  watch: 'ytd-compact-video-renderer',
-  channel: 'ytd-rich-item-renderer',
-}
-
-export const videoMetaDataQuerySelector = {
-  home: '#content ytd-rich-grid-media #dismissible #details #meta',
-  watch: '#dismissible .metadata',
 }
